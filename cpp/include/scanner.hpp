f5afc6db155964820dacb8ffeec46cdd77daf923
StevenShen <761567595@qq.com>
2019-09-19 12:14:26 +0800

    Added the Copy Constructor of Token

--- cpp/include/scanner.hpp
+++ cpp/include/scanner.hpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <memory>
 
 #include "Compiler.hpp"
 
@@ -16,16 +18,17 @@ class Scanner;
 extern int scan(std::ostream &os = std::cout);
 // TODO Make the overload of print function is friendly.
 extern std::ostream &operator<<(std::ostream &, const Token &);
-extern bool isLexeme(Mark);
 extern bool numeric(const Token&);
 extern Token max(const Token&, const Token&);
 
-/* Class Token is immutable. */
+/* Mark the basic data type. */
+extern const Token Int, Real, Char, Bool;
+
+/* Mark the basic reserved words. */
+extern const Token And, Or, eq, ne, le, ge, minus, True, False, temp;
+
 class Token {
 public:
-    const Mark tot; 
-    const int width; // For make the token to contains Type.
-
     /* Constructor of Token. */
     Token(Mark tot) : tot(tot), width(0), ival(0) { }
 
@@ -35,18 +38,29 @@ public:
     Token(double dval)      : tot(Mark::CREAL), width(0), dval(dval) { }
     Token(std::string sval) : tot(Mark::CSTR),  width(0), sval(sval) { }
 
+    // TODO Add the checkRep for the below two constructors! In case of destructor 
+    // could not recovery the memory!
     Token(std::string lexeme, Mark mark) : tot(mark), width(0), lexeme(lexeme) { }
-
     Token(std::string s, Mark m, int w): tot(m), width(w), lexeme(s) { }
 
-    // TODO Implement the copy constructor!
+    /* Copy Constructor. */
+    Token(const Token &tk) : tot(tk.tot), width(tk.width) { copyUnion(tk); }
+    Token &operator=(const Token&);
 
     /* Destructor of Token. */
     ~Token() { if (tot == Mark::CSTR) sval.~basic_string(); 
-               else if (isLexeme(tot)) lexeme.~basic_string(); }
+               else if (isLexeme()) lexeme.~basic_string(); }
+
+    /* Methods to get token information. */
+    int getTot() const;
+    std::string getLexeme() const;
+    int getWidth() const;
+    bool isLexeme() const;
 
 private:
-    const union {
+    Mark tot; 
+    int width; // For make the token to contains Type.
+    union {
         int ival;
         bool bval;
         char cval;
@@ -54,12 +68,21 @@ private:
         std::string sval;
         std::string lexeme;
     };
+    void copyUnion(const Token&);
 };
 
-/* Mark the basic data type. */
-extern const Token Int, Real, Char, Bool;
+/* Scanner is used to scan the source file, and get the recognized tokens. */
+class Scanner {
+public:
+    static int line;
+    char peek = ' ';
+    std::shared_ptr<std::map<std::string, Token>> words;
 
-/* Mark the basic reserved words. */
-extern const Token And, Or, eq, ne, le, ge, minus, True, False, temp;
+    // Check the token whether is lexeme.
+    void reserve(Token tk);
+
+    Scanner() {
+    }
+};
 
 #endif /* __SCANNER_HPP */
--- cpp/src/SymbolTable.cpp
+++
@@ -1,6 +0,0 @@
-// Name   : SymbolTable.cpp
-// Author : Modnar
-// Date   : 2019-05-21
-// Copyright (C) 2019 Modnar. All rights reserved.
-
-#include "include/SymbolTable.hpp"
